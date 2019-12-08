ac45c05a4157fe5bd88dfa2181d34316ec8e842c
kejaly <46043605+kejaly@users.noreply.github.com>
2019-12-08 23:18:37 +0800

    添加遗漏的ZhibiaoModel.php

---
+++ application/models/ZhiBiaoModel.php
@@ -0,0 +1,15 @@
+<?php
+class ZhiBiaoModel extends CI_Model{
+    public function __construct(){
+        parent::__construct();
+        $this->load->database();
+        $this->load->library('session');
+    }
+
+    //插入数据到ZhiBiao 表中
+    public function insert(
+
+    ){
+
+    }
+}
